6457e63321240c5cac76b741d3701a2ce0c6e662
maxxwell131 <maxxwell131@yahoo.com>
2018-01-01 16:49:45 +0200

    first example

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -9,33 +9,8 @@ namespace Snake
     {
         public static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            Point p2 = new Point(4, 5, '#');
-            Point p3 = new Point(21, 23, '@');
-            Point p4 = new Point(33, 44, '%');
-
-
-            List<Point> pointList = new List<Point>();
-            pointList.Add(p1);
-            pointList.Add(p2);
-            pointList.Add(p3);
-            pointList.Add(p4);
-            pointList.Add(p1);
-
-            foreach(Point obj in pointList)
-            {
-                obj.pointPrint();
-            }
-
-            pointList.RemoveAt(pointList.Count - 1);
-
-            Console.WriteLine(new string('*', 30));
-
-            foreach (Point obj in pointList)
-            {
-                obj.pointPrint();
-            }
-
+            HorizontalLine hLine = new HorizontalLine();
+            hLine.Drow();
 
             Console.ReadLine();
         }
