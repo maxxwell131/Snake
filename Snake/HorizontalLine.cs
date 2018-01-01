206402f6fa313bb80f4d3a4f91b71dffba0bf905
maxxwell131 <maxxwell131@yahoo.com>
2018-01-01 23:25:31 +0200

    Horizontal & Vertical line class added

---
+++ Snake/HorizontalLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symb)
+        {
+            pList = new List<Point>();
+            for(int i = xLeft; i <= xRight; i++)
+            {
+                Point p = new Point(i, y, symb);
+                pList.Add(p);
+            }
+           
+        }
+
+        public void Drow()
+        {
+            foreach(Point obj in pList)
+            {
+                //obj.pointPrint();
+                obj.Draw();
+            }
+        }
+    }
+}
---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int x, int yStart, int yFinish, char symb)
+        {
+            pList = new List<Point>();
+
+            for(int i = yStart; i <= yFinish; i++)
+            {
+                Point p = new Point(x, i, symb);
+                pList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach (Point obj in pList)
+            {
+                //obj.pointPrint();
+                obj.Draw();
+            }
+        }
+    }
+}
